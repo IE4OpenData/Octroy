1547539a8bd933e96c5da2d337d0e7d3933042cc
Pablo Duboue <pablo.duboue@gmail.com>
2016-07-21 10:51:54 -0400

    class4 wip

---
+++ src/main/java/org/ie4opendata/octroy/ReasonAnnotator.java
@@ -0,0 +1,121 @@
+package org.ie4opendata.octroy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.descriptor.TypeCapability;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.cleartk.ml.CleartkSequenceAnnotator;
+import org.cleartk.ml.Feature;
+import org.cleartk.ml.Instances;
+import org.cleartk.ml.chunking.BioChunking;
+import org.cleartk.ml.feature.extractor.CleartkExtractor;
+import org.cleartk.ml.feature.extractor.CleartkExtractor.Following;
+import org.cleartk.ml.feature.extractor.CleartkExtractor.Preceding;
+import org.cleartk.ml.feature.extractor.CombinedExtractor1;
+import org.cleartk.ml.feature.extractor.CoveredTextExtractor;
+import org.cleartk.ml.feature.extractor.FeatureExtractor1;
+import org.cleartk.ml.feature.function.CharacterCategoryPatternFunction;
+import org.cleartk.ml.feature.function.FeatureFunctionExtractor;
+import org.cleartk.ml.jar.DefaultSequenceDataWriterFactory;
+import org.cleartk.ml.jar.DirectoryDataWriterFactory;
+import org.cleartk.ml.jar.GenericJarClassifierFactory;
+import org.cleartk.ml.mallet.MalletCrfStringOutcomeDataWriter;
+import org.uimafit.util.JCasUtil;
+
+@TypeCapability(outputs = { "org.ie4opendata.octroy.Reason" }, inputs = { "org.ie4opendata.octroy.Token", "org.ie4opendata.octroy.Sentence",
+        "org.ie4opendata.octroy.DocumentAnnotation",
+        "org.ie4opendata.octroy.Company",
+        "org.ie4opendata.octroy.Amount" })
+public class ReasonAnnotator extends CleartkSequenceAnnotator<String> {
+
+	private FeatureExtractor1 extractor;
+
+	private CleartkExtractor contextExtractor;
+
+	private BioChunking<Token, Reason> chunking;
+
+	@Override
+	public void initialize(UimaContext context) throws ResourceInitializationException {
+		super.initialize(context);
+
+		// the token feature extractor: text, char pattern (uppercase, digits,
+		// etc.), and part-of-speech
+		this.extractor = new CombinedExtractor1(new CoveredTextExtractor(),
+				new FeatureFunctionExtractor(new CoveredTextExtractor(),
+						new CharacterCategoryPatternFunction(
+								CharacterCategoryPatternFunction.PatternType.REPEATS_MERGED))
+				/*, new TypePathExtractor(Token.class, "pos")*/);
+
+		// the context feature extractor: the features above for the 3 preceding
+		// and 3 following tokens
+		this.contextExtractor = new CleartkExtractor(Token.class, this.extractor, new Preceding(3), new Following(3));
+
+		// the chunking definition: Tokens will be combined to form EntityLevel
+		// annotation, with labels from the "entityType" attribute so that we
+		// get B-location, I-person, etc.
+		this.chunking = new BioChunking<Token, Reason>(Token.class, Reason.class, "entityType");
+	}
+
+	@Override
+	public void process(JCas jCas) throws AnalysisEngineProcessException {
+		for (Sentence sentence : JCasUtil.select(jCas, Sentence.class)) {
+
+			// extract features for each token in the sentence
+			List<Token> tokens = JCasUtil.selectCovered(jCas, Token.class, sentence);
+			List<List<Feature>> featureLists = new ArrayList<List<Feature>>();
+			for (Token token : tokens) {
+				List<Feature> features = new ArrayList<Feature>();
+				features.addAll(this.extractor.extract(jCas, token));
+				features.addAll(this.contextExtractor.extract(jCas, token));
+				featureLists.add(features);
+			}
+			// during training, convert NamedEntityMentions in the CAS into
+			// expected classifier outcomes
+			if (this.isTraining()) {
+
+				// extract the gold (human annotated) NamedEntityMention
+				// annotations
+				List<Reason> entityLevel = JCasUtil.selectCovered(jCas, Reason.class, sentence);
+
+				// convert the NamedEntityMention annotations into token-level
+				// BIO outcome labels
+				List<String> outcomes = this.chunking.createOutcomes(jCas, tokens, entityLevel);
+
+				// write the features and outcomes as training instances
+				this.dataWriter.write(Instances.toInstances(outcomes, featureLists));
+			}
+
+			// during classification, convert classifier outcomes into
+			// NamedEntityMentions in the CAS
+			else {
+
+				// get the predicted BIO outcome labels from the classifier
+				List<String> outcomes = this.classifier.classify(featureLists);
+
+				// create the NamedEntityMention annotations in the CAS
+				this.chunking.createChunks(jCas, tokens, outcomes);
+			}
+		}
+	}
+	
+    public static AnalysisEngineDescription getWriterDescription(String outputDirectory)
+            throws ResourceInitializationException {
+        return AnalysisEngineFactory.createPrimitiveDescription(ReasonAnnotator.class,
+                CleartkSequenceAnnotator.PARAM_IS_TRAINING, true, DirectoryDataWriterFactory.PARAM_OUTPUT_DIRECTORY,
+                outputDirectory, DefaultSequenceDataWriterFactory.PARAM_DATA_WRITER_CLASS_NAME,
+                MalletCrfStringOutcomeDataWriter.class);
+    }
+
+    public static AnalysisEngineDescription getClassifierDescription(String modelFileName)
+            throws ResourceInitializationException {
+        return AnalysisEngineFactory.createPrimitiveDescription(ReasonAnnotator.class,
+                GenericJarClassifierFactory.PARAM_CLASSIFIER_JAR_PATH, modelFileName);
+    }
+
+}
---
+++ src/main/java/org/ie4opendata/octroy/apps/ReasonTrainer.java
@@ -0,0 +1,40 @@
+package org.ie4opendata.octroy.apps;
+
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.fit.factory.AggregateBuilder;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.util.XMLInputSource;
+import org.cleartk.ml.jar.Train;
+import org.cleartk.util.cr.XReader;
+import org.ie4opendata.octroy.ReasonAnnotator;
+
+/**
+ * Based on http://code.google.com/p/cleartk/wiki/TutorialPartOfSpeechClassifier
+ * 
+ */
+public class ReasonTrainer {
+    public static void main(String[] args) throws Exception {
+        // A collection reader that reads XMIs
+        CollectionReader reader = CollectionReaderFactory.createCollectionReader(CollectionReaderFactory
+                .createDescription(XReader.class, null, XReader.PARAM_ROOT_FILE, args[0]));
+
+        // The pipeline of annotators
+        AggregateBuilder builder = new AggregateBuilder();
+
+//        other annotators, if needed
+//        builder.add(UIMAFramework.getXMLParser().parseAnalysisEngineDescription(
+//                new XMLInputSource("src/main/resources/org/apache/opennlp/OpenNlpSentTokNerPos.xml")));
+
+        // The reason classifier annotator, configured to write training data
+        builder.add(ReasonAnnotator.getWriterDescription("train_data"));
+
+        // Run the pipeline of annotators on each of the CASes produced by the reader
+        SimplePipeline.runPipeline(reader, builder.createAggregateDescription());
+
+        // Train a classifier on the training data, and package it into a .jar file
+        Train.main("train_data");
+    }
+}
+
