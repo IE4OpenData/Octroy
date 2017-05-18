4ce782a77143b0c9817c5f57688b0facaa003c37
Pablo Duboue <pablo.duboue@gmail.com>
2017-05-18 13:41:46 -0400

    Changes from class2

--- src/main/java/org/ie4opendata/octroy/TsvWriter.java
+++ src/main/java/org/ie4opendata/octroy/TsvWriter.java
@@ -24,105 +24,119 @@ import org.cleartk.util.ViewUriUtil;
 
 public class TsvWriter extends JCasAnnotator_ImplBase {
 
-  public static AnalysisEngineDescription getDescription(File outputDirectory)
-          throws ResourceInitializationException {
-    return AnalysisEngineFactory.createEngineDescription(TsvWriter.class,
-            TsvWriter.PARAM_OUTPUT_FILE, outputDirectory);
-  }
+	public static AnalysisEngineDescription getDescription(File outputDirectory)
+			throws ResourceInitializationException {
+		return AnalysisEngineFactory.createEngineDescription(TsvWriter.class, TsvWriter.PARAM_OUTPUT_FILE,
+				outputDirectory);
+	}
 
-  @Override
-  public void initialize(UimaContext context) throws ResourceInitializationException {
-    super.initialize(context);
-    PrintWriter pw;
-    try {
-      pw = new PrintWriter(new FileWriter(outputFile));
-      pw.println("Document\tAmount\tAmount Start\tAmount End\tCompany\tCompany Start\tCompany End\tReason\tReason Start\tReason End");
-      pw.close();
-    } catch (IOException e) {
-      throw new ResourceInitializationException(e);
-    }
-  }
+	@Override
+	public void initialize(UimaContext context) throws ResourceInitializationException {
+		super.initialize(context);
+		PrintWriter pw;
+		try {
+			pw = new PrintWriter(new FileWriter(outputFile));
+			pw.println("Document\tAmount\tAmount Start\tAmount End\t"
+					+ "Company\tCompany Start\tCompany End\tNEQ\tOfficial Name\t" + "Reason\tReason Start\tReason End");
+			pw.close();
+		} catch (IOException e) {
+			throw new ResourceInitializationException(e);
+		}
+	}
 
-  public static final String PARAM_OUTPUT_FILE = "outputDirectory";
+	public static final String PARAM_OUTPUT_FILE = "outputDirectory";
 
-  @ConfigurationParameter(name = PARAM_OUTPUT_FILE)
-  protected File outputFile;
+	@ConfigurationParameter(name = PARAM_OUTPUT_FILE)
+	protected File outputFile;
 
-  @Override
-  public void process(JCas jCas) throws AnalysisEngineProcessException {
-    try {
-      PrintWriter pw = new PrintWriter(new FileWriter(outputFile, true)); // slow, but effective
-      String doc = ViewUriUtil.getURI(jCas).getFragment();
-      boolean printed = false;
-      for (Amount amt : JCasUtil.select(jCas, Amount.class)) {
-        boolean printHere = false;
-        for (Company cmp : JCasUtil.select(jCas, Company.class)) {
-          boolean printCmp = false;
-          for (Reason rsn : JCasUtil.select(jCas, Reason.class)) {
-            pw.print(doc);
-            printAnnotation(pw, amt);
-            printAnnotation(pw, cmp);
-            printAnnotation(pw, rsn);
-            pw.println();
-            printCmp = true;
-          }
-          if (!printCmp) {
-            pw.print(doc);
-            printAnnotation(pw, amt);
-            printAnnotation(pw, cmp);
-            printAnnotation(pw, null);
-            pw.println();
-          }
-          printHere = true;
-        }
-        if (!printHere) {
-          pw.print(doc);
-          printAnnotation(pw, amt);
-          printAnnotation(pw, null);
-          printAnnotation(pw, null);
-          pw.println();
-        }
-        printed = true;
-      }
-      if (!printed) // no amounts
-        for (Company cmp : JCasUtil.select(jCas, Company.class)) {
-          boolean printCmp = false;
-          for (Reason rsn : JCasUtil.select(jCas, Reason.class)) {
-            pw.print(doc);
-            printAnnotation(pw, null);
-            printAnnotation(pw, cmp);
-            printAnnotation(pw, rsn);
-            pw.println();
-            printCmp = true;
-          }
-          if (!printCmp) {
-            pw.print(doc);
-            printAnnotation(pw, null);
-            printAnnotation(pw, cmp);
-            printAnnotation(pw, null);
-            pw.println();
-          }
-          printed = true;
-        }
-      if (!printed) // no amounts, no companies
-        for (Reason rsn : JCasUtil.select(jCas, Reason.class)) {
-          pw.print(doc);
-          printAnnotation(pw, null);
-          printAnnotation(pw, null);
-          printAnnotation(pw, rsn);
-          pw.println();
-        }
-      pw.close();
-    } catch (IOException e) {
-      throw new AnalysisEngineProcessException(e);
-    }
-  }
+	@Override
+	public void process(JCas jCas) throws AnalysisEngineProcessException {
+		try {
+			PrintWriter pw = new PrintWriter(new FileWriter(outputFile, true)); // slow,
+																				// but
+																				// effective
+			String doc = ViewUriUtil.getURI(jCas).getFragment();
+			boolean printed = false;
+			for (Amount amt : JCasUtil.select(jCas, Amount.class)) {
+				boolean printHere = false;
+				for (Company cmp : JCasUtil.select(jCas, Company.class)) {
+					boolean printCmp = false;
+					for (Reason rsn : JCasUtil.select(jCas, Reason.class)) {
+						pw.print(doc);
+						printAnnotation(pw, amt);
+						printAnnotation(pw, cmp);
+						printAnnotation(pw, rsn);
+						pw.println();
+						printCmp = true;
+					}
+					if (!printCmp) {
+						pw.print(doc);
+						printAnnotation(pw, amt);
+						printAnnotation(pw, cmp);
+						printAnnotation(pw, null);
+						pw.println();
+					}
+					printHere = true;
+				}
+				if (!printHere) {
+					pw.print(doc);
+					printAnnotation(pw, amt);
+					printAnnotation(pw, null);
+					printAnnotation(pw, null);
+					pw.println();
+				}
+				printed = true;
+			}
+			if (!printed) // no amounts
+				for (Company cmp : JCasUtil.select(jCas, Company.class)) {
+					boolean printCmp = false;
+					for (Reason rsn : JCasUtil.select(jCas, Reason.class)) {
+						pw.print(doc);
+						printAnnotation(pw, null);
+						printAnnotation(pw, cmp);
+						printAnnotation(pw, rsn);
+						pw.println();
+						printCmp = true;
+					}
+					if (!printCmp) {
+						pw.print(doc);
+						printAnnotation(pw, null);
+						printAnnotation(pw, cmp);
+						printAnnotation(pw, null);
+						pw.println();
+					}
+					printed = true;
+				}
+			if (!printed) // no amounts, no companies
+				for (Reason rsn : JCasUtil.select(jCas, Reason.class)) {
+					pw.print(doc);
+					printAnnotation(pw, null);
+					printAnnotation(pw, null);
+					printAnnotation(pw, rsn);
+					pw.println();
+				}
+			pw.close();
+		} catch (IOException e) {
+			throw new AnalysisEngineProcessException(e);
+		}
+	}
 
-  private void printAnnotation(PrintWriter pw, Annotation ann) throws IOException {
-    if (ann == null)
-      pw.print("\t\t\t");
-    else
-      pw.print("\t" + ann.getCoveredText().replaceAll("\n", " ").replaceAll("\t", " ").trim() + "\t"
-              + ann.getBegin() + "\t" + ann.getEnd());
-  }
+	private void printAnnotation(PrintWriter pw, Annotation ann) throws IOException {
+		if (ann == null)
+			pw.print("\t\t\t");
+		else {
+			pw.print("\t" + ann.getCoveredText().replaceAll("\n", " ").replaceAll("\t", " ").trim() + "\t"
+					+ ann.getBegin() + "\t" + ann.getEnd());
+			if (ann instanceof Company) {
+				Company cmp = (Company) ann;
+				String officialName = cmp.getOfficialName();
+				String registrationNumber = cmp.getRegistrationNumber();
+				if (officialName == null)
+					officialName = "";
+				if (registrationNumber == null)
+					registrationNumber = "";
+				pw.print("\t" + registrationNumber + "\t" + officialName);
+			}
+		}
+	}
 }
